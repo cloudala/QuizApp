5d5e73a22159607783b0a9cbba7ddc22d2c53f0c
Alicja Chmura <ala.chmura2003@gmail.com>
2024-01-26 02:52:25 +0100

    Added bcrypt library to project and implemented password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,34 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-const UserModel = mongoose.model("users", UserSchema)
+// Hash the password before saving
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
 
-module.exports = UserModel
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,34 +2,30 @@ const express = require('express');
 const router = express.Router();
 const UserModel = require('../models/User');
 
-router.post('/api/register', (req, res) => {
-    UserModel.create(req.body)
-      .then((user) => {
-        res.status(201).json(user);
-      })
-      .catch((err) => {
-          res.status(500).json("Internal Server Error");
-      });
+router.post('/api/register', async (req, res) => {
+  try {
+    const user = new UserModel(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json("Internal Server Error");
+  }
 });
   
-router.post('/api/login', (req, res) => {
+router.post('/api/login', async (req, res) => {
     const { email, password } = req.body;
   
-    UserModel.findOne({ email: email })
-      .then((user) => {
-        if (user) {
-          if (user.password === password) {
-            res.status(200).json("Success!");
-          } else {
-            res.status(401).json("Wrong password!");
-          }
-        } else {
-          res.status(404).json("User not found!");
-        }
-      })
-      .catch((error) => {
-        res.status(500).json("Internal Server Error");
-      });
+    try {
+      const user = await UserModel.findOne({ email: email });
+  
+      if (user && await user.comparePassword(password)) {
+        res.status(200).json("Success!");
+      } else {
+        res.status(401).json("Wrong password!");
+      }
+    } catch (error) {
+      res.status(500).json("Internal Server Error");
+    }
 });
 
 module.exports = router;
