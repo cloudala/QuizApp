fc890965cbbcd239250a3818f67fae7a02ab6f05
Alicja Chmura <ala.chmura2003@gmail.com>
2024-01-28 21:57:35 +0100

    Category and quiz panel refactor - only logged in users can manage categories/quizzes

--- client/src/pages/CategoriesPage.js
+++ client/src/pages/CategoriesPage.js
@@ -1,31 +1,37 @@
 import React, { useContext } from 'react';
 import { CategoryContext } from '../contexts/CategoryContext';
-// import QuizSearch from '../components/QuizSearch';
+import { UserContext } from '../contexts/UserContext';
 import AddCategoryForm from '../components/AddCategoryForm'
 import CategoryList from '../components/CategoryList';
 import Loading from '../components/Loading';
-// import UserData from '../components/UserData';
 
 export default function CategoriesPage() {
-    const { categories, loading, error, fetchData } = useContext(CategoryContext);
-    return (
-        <div className='min-h-screen'>
+  const {user} = useContext(UserContext)
+  const { categories, loading, error, fetchData } = useContext(CategoryContext);
+  return (
+    <div className='min-h-screen'>
+      {user ? (
+        <>
           {loading ? (
-            <Loading/>
+            <Loading />
           ) : !error ? (
             <>
-            <div>
-              <h1 className='text-2xl font-semibold mt-5 mx-3'>Add Category</h1>
-              <AddCategoryForm/>
-            </div>
-            <div>
-              <h1 className='text-2xl font-semibold my-5 mx-3'>Manage Categories</h1>
-              <CategoryList categories={categories}/>
-            </div>
+              <div>
+                <h1 className='text-2xl font-semibold mt-5 mx-3'>Add Category</h1>
+                <AddCategoryForm />
+              </div>
+              <div>
+                <h1 className='text-2xl font-semibold my-5 mx-3'>Manage Categories</h1>
+                <CategoryList categories={categories} />
+              </div>
             </>
           ) : (
             <p>Error fetching data</p>
           )}
-        </div>
-      );
+        </>
+      ) : (
+        <p className="text-xl mt-5 mx-3 font-semibold">You have to be logged in to manage categories.</p>
+      )}
+    </div>
+  );
 }
--- client/src/pages/ManageQuizPage.js
+++ client/src/pages/ManageQuizPage.js
@@ -1,21 +1,28 @@
 import React, {useContext} from 'react'
 import { QuizContext } from '../contexts/QuizContext'
+import { UserContext } from '../contexts/UserContext';
 import QuizForm from "../components/QuizForm"
 import ManageQuizList from "../components/ManageQuizList"
 
-
 export default function ManageQuizPage() {
+    const {user} = useContext(UserContext)
     const {quizzes} = useContext(QuizContext)
     return (
         <div className="min-h-screen">
-            <div className="flex flex-col justify-center items-center">
-                <h1 className='text-2xl font-semibold my-5 mx-3'>Add Quiz</h1>
-                <QuizForm/>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-                <h1 className='text-2xl font-semibold my-5 mx-3'>Manage Quizzes</h1>
-                <ManageQuizList quizzes={quizzes}/>
-            </div>
+            {user ?  
+            <>
+                <div className="flex flex-col justify-center items-center">
+                    <h1 className='text-2xl font-semibold my-5 mx-3'>Add Quiz</h1>
+                    <QuizForm/>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <h1 className='text-2xl font-semibold my-5 mx-3'>Manage Quizzes</h1>
+                    <ManageQuizList quizzes={quizzes}/>
+                </div>
+            </>
+            :
+            <p className="text-xl mt-5 mx-3 font-semibold">You have to be logged in to manage quizzes.</p>
+            }
         </div>
     )
 }
