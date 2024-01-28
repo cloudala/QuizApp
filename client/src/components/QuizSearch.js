bfa5581e735abdace0f38e2d86a3a6694f417012
Alicja Chmura <ala.chmura2003@gmail.com>
2024-01-28 17:38:20 +0100

    Refactored quiz search component ui and functionality

--- client/src/components/QuizSearch.js
+++ client/src/components/QuizSearch.js
@@ -18,6 +18,11 @@ const QuizSearch = () => {
     }
   };
 
+  const handleHideQuizzes = () => {
+    setFoundQuizzes(null);
+    setSearchQuery('');
+  };
+
   return (
     <div className="mx-auto p-4 bg-white rounded-md shadow-md">
       <div className="flex items-center">
@@ -30,10 +35,18 @@ const QuizSearch = () => {
         />
         <button
           onClick={handleSearch}
-          className="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-r-lg text-sm px-5 py-2.5 text-center"
         >
           Search
         </button>
+        {foundQuizzes && (
+          <button
+            onClick={handleHideQuizzes}
+            className="text-blue-800 bg-blue-100 hover:bg-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center h-10 ml-3"
+          >
+            Hide
+          </button>
+        )}
       </div>
 
       {loading && <p className="mt-4 text-gray-600">Loading...</p>}
